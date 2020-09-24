8596b1999e86cad715ce886ab525bef7f1aa6aec
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-09-24 12:51:16 -0400

    Added check for allowEscClose before close modal

--- src/born-modal.js
+++ src/born-modal.js
@@ -347,7 +347,7 @@ export default class Modal {
             isCloseTarget = evt.target.hasAttribute('data-modal-close');
             isCloseAllTarget = evt.target.hasAttribute('data-modal-close-all');
             wasClick = evt.type === 'click' && ((evt.target === targetModal && targetModal.modal.options.allowClickOutClose) || isCloseTarget || isCloseAllTarget);
-            wasEsc   = document.activeElement.tagName !== 'INPUT' && evt.keyCode === 27;
+            wasEsc   = document.activeElement.tagName !== 'INPUT' && evt.keyCode === 27 && targetModal.modal.options.allowEscClose;
 
             canClose = wasClick || wasEsc;
         }
